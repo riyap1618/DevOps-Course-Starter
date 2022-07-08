b959252fef3a90f49ac8a4518739fadddd70d0cc
Riya Patel <riya.patel@softwire.com>
2022-07-08 11:02:55 +0100

    With stretch goals

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,7 +4,7 @@ from flask import redirect, request
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
-from todo_app.data.session_items import add_item
+from todo_app.data.session_items import add_item, save_item, remove_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -18,6 +18,23 @@ def index():
 
 @app.route('/', methods=['POST'])
 def add():
-    title = request.form.get('title')
-    item = add_item(title)
+    if request.form.get('option') == "add":
+        title = request.form.get('title')
+        item = add_item(title)
+    elif request.form.get('option') == 'start':
+        title = request.form.get('title')
+        for x in get_items():
+            if x['title'] == title:
+                new_item = { 'id': x['id'], 'title': title, 'status': 'Started' }
+                save_item(new_item)
+    elif request.form.get('option') == 'complete':
+        title = request.form.get('title')
+        for x in get_items():
+            if x['title'] == title:
+                new_item = { 'id': x['id'], 'title': title, 'status': 'Completed' }
+                save_item(new_item)
+    elif request.form.get('option') == 'delete':
+        title = request.form.get('title')
+        remove_item(title)
     return redirect("http://localhost:5000/")
+
