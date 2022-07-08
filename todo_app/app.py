f18110debbc803631bb120311385272d0a15718f
Riya Patel <riya.patel@softwire.com>
2022-07-08 09:40:35 +0100

    Exercise 1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import render_template
+from flask import redirect, request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +12,12 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    print(get_items())
+    return render_template('index.html', listOfItems=get_items())
+    #return 'Hello World!'
+
+@app.route('/', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    item = add_item(title)
+    return redirect("http://localhost:5000/")
